1caea6212496ba92e504fd8b4129986b2b8e02e9
WoodyTheOriginal <mathieu1.guignard@gmail.com>
2023-12-03 19:42:27 +0100

    fjdkslafjldsa

--- src/secrets_things.py
+++
@@ -1,5 +0,0 @@
-API_KEYS = [
-    "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",
-    "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",
-    "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",
-]
